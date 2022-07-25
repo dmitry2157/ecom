e0313f7923c1e6916ab00205c80ee7e5de155364
dmitrypotekhin <dmpotekhin@yandex.ru>
2022-07-25 18:07:11 +0300

    ecom

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:app_frontend/pages/signup.dart';
+import 'package:app_frontend/pages/login.dart';
+import 'package:app_frontend/pages/start.dart';
+import 'package:app_frontend/pages/home.dart';
+import 'package:app_frontend/components/shop.dart';
+import 'package:app_frontend/pages/products/items.dart';
+import 'package:app_frontend/pages/products/subCategory.dart';
+import 'package:app_frontend/pages/shoppingBag.dart';
+import 'package:app_frontend/pages/checkout/addCreditCard.dart';
+import 'package:app_frontend/pages/checkout/paymentMethod.dart';
+import 'package:app_frontend/pages/checkout/shippingAddress.dart';
+import 'package:app_frontend/pages/checkout/shippingMethod.dart';
+import 'package:app_frontend/pages/products/particularItem.dart';
+import 'package:app_frontend/pages/checkout/placeOrder.dart';
+import 'package:app_frontend/pages/profile/userProfile.dart';
+import 'package:app_frontend/pages/profile/editProfile.dart';
+import 'package:app_frontend/pages/profile/setting.dart';
+import 'package:app_frontend/pages/profile/contactUs.dart';
+import 'package:app_frontend/pages/products/wishlist.dart';
+import 'package:app_frontend/components/orders/orderHistory.dart';
+import 'package:app_frontend/pages/onBoardingScreen/onboardingScreen.dart';
+import 'package:app_frontend/pages/adminPanel.dart';
+
+bool firstTime;
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  firstTime = (prefs.getBool('initScreen') ?? false);
+  if(!firstTime){
+    prefs.setBool('initScreen', true);
+  }
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    Main()
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      initialRoute: firstTime ? '/': '/onBoarding',
+      routes: {
+        '/': (context) => Start(),
+        '/login': (context) => Login(),
+        '/signup': (context) => SignUp(),
+        '/home': (context) => Home(),
+        '/shop': (context) => Shop(),
+        '/subCategory': (context) => SubCategory(),
+        '/items': (context) => Items(),
+        '/particularItem': (context) => ParticularItem(),
+        '/bag': (context) => ShoppingBag(),
+        '/wishlist': (context) => WishList(),
+        '/checkout/addCreditCard': (context) => AddCreditCard(),
+        '/checkout/address': (context) => ShippingAddress(),
+        '/checkout/shippingMethod': (context) => ShippingMethod(),
+        '/checkout/paymentMethod': (context) => PaymentMethod(),
+        '/checkout/placeOrder': (context) => PlaceOrder(),
+        '/profile': (context) => UserProfile(),
+        '/profile/settings': (context) => ProfileSetting(),
+        '/profile/edit': (context) => EditProfile(),
+        '/profile/contactUs': (context) => ContactUs(),
+        '/placedOrder': (context) => OrderHistory(),
+        "/onBoarding": (context) => OnBoardingScreen(),
+        "/admin": (context) => AdminPanel()
+      },
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        bottomSheetTheme: BottomSheetThemeData(
+          backgroundColor: Colors.white
+        )
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
